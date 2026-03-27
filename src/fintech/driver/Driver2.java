64b85cf29b6fe281f776dcd07596a7b82bbedec1
DeaHutapea <deahutapea99@gmail.com>
2026-03-27 17:44:46 +0700

    Update Driver2 to handle transaction creation
    Refactored Driver2 class to support transaction creation.

--- src/fintech/driver/Driver2.java
+++ src/fintech/driver/Driver2.java
@@ -1,15 +1,30 @@
-package academic.driver;
+package fintech.driver;
 
-/**
- * @author NIM Nama
- * @author NIM Nama
- */
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Scanner;
+import fintech.model.Account;
+import fintech.model.Transaction;
+import fintech.model.TransferTransaction;
+
 public class Driver2 {
 
-    public static void main(String[] _args) {
-
-        // codes
-
+    public static List<Transaction> createTransactions(String[] commands, List<Account> accounts) {
+        List<Transaction> transactions = new ArrayList<>();
+        for (String command : commands) {
+            String[] tokens = command.split("#");
+            if (tokens[0].equals("create-transaction")) {
+                Account account = findAccount(tokens[1], accounts);
+                if (account != null) {
+                    double amount = Double.parseDouble(tokens[2]);
+                    String postedAt = tokens[3];
+                    String note = tokens[4];
+                    Transaction transaction = new Transaction(account, amount, postedAt, note);
+                    account.addTransaction(transaction);
+                    transactions.add(transaction);
+                }
+            }
+        }
+        return transactions;
     }
-
 }
